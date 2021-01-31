c7c6154c3d70e70399901c135d1af2de35ec6abd
William Zimrin <wzimrin@pop-os.localdomain>
2021-01-31 18:21:00 -0500

    Hello World

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App(): JSX.Element {
+    return <div>Hello World</div>;
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
